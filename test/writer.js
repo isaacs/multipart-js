c2ba6cf191918067b35b9452b16f2378feafabba
John Wright <mrjjwright@gmail.com>
2010-04-05 11:18:39 -0600

    writing headers

--- test/writer.js
+++ test/writer.js
@@ -13,13 +13,18 @@ function output(msg) {
 }
 
 // Some simple parts we will use
-var goodSimplePart = { "content-disposition": "multipart-form"
+var goodSimpleParts = [
+{ headers: { "content-disposition": "multipart-form"
 , "content-type": "text/plain"
 , "filename": "foo.txt"
-}, badSimplePart = { "content-disposition": "multipart-form"
+}, body: "", encoded: "xx"}];
+
+var badSimpleParts = [	{
+headers: { "missing-content-disposition": "multipart-form"
 , "content-type": "text/plain"
 , "filename": "foo.txt"
-};
+}, body: "--boundary\r\n  fail: blahrg\r\n\r\n"}];
+
 
 errorHandlerCalled = false;
 errorMessage = "";
@@ -37,25 +42,43 @@ function handleData (chunk) {
 	output("emitted data");
 }
 
+function checkForError(writer) {
+  if (!writer.error) {
+    sys.debug("Good, no errors on writer.");
+  } else {
+    assert.ok(false,  writer.error.message);
+  }
+}
+
 writer.onerror = handleError;
 writer.ondata = handleData;
 
-sys.debug("Test some general error handling.")
-sys.debug("Wrong - write a part without setting boundary...");
-writer.partBegin(goodSimplePart);
-assert.ok(errorHandlerCalled, "should emit onerror if part written without boundary");
+sys.debug("Write the first part.  A simple good part.")
+sys.debug("")
 
-sys.debug("Fix the last error.  Set the boundary property.");
-writer.boundary  = "boundary";
-sys.debug("But leave the error object...")
+sys.debug("Wrong - write the part without setting boundary or type...");
+writer.partBegin(goodSimpleParts[0].headers);
+assert.ok(errorHandlerCalled, "Should error if type or boundary properties are not set.");
+
+sys.debug("Set the type property.");
+writer.type  = "multipart/form-data";
 try {
-	writer.partBegin(goodSimplePart);
-	assert.ok(false, "should throw error if not cleaned up from last call");
+	writer.partBegin(goodSimpleParts[0].headers);
+	assert.ok(false, "Should throw error if not cleaned up from last call.");
 }
 catch (error) {
 	output("threw: " + error.message);
 }
-
-sys.debug("Cleanup the error. Set the error to null.");
+sys.debug("Whoops, forgot to cleanup. Set the error to null. Try again...");
 writer.error = null;
-writer.partBegin(goodSimplePart);
+writer.partBegin(goodSimpleParts[0].headers);
+assert.notEqual(writer.error, null, "Should error if boundary or type properties are not set");
+writer.error = null;
+sys.debug("Set boundary.  Try again...")
+writer.boundary  = "boundary";
+writer.partBegin(goodSimpleParts[0].headers);
+checkForError(writer);
+sys.debug("")
+sys.debug("Were the http headers plus first part emitted and encoded properly?")
+assert.equal(goodSimpleParts[0].encoded, lastChunk, "Should emit and encode first part properly.")
+
